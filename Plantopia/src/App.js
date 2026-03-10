72abea0a5e378d65a6bc939e56215ef36a02c88b
jimnorrisSA <jim.norris@soulassembly.com>
2026-03-10 15:24:40 +0000

    change background

--- Plantopia/src/App.js
+++ Plantopia/src/App.js
@@ -1418,7 +1418,7 @@ function UsernameScreen({ onSet }) {
   return (
     <div
       style={{
-        background: T.bg,
+       background: "repeating-linear-gradient(135deg, #fffbf0, #fffbf0 20px, #f0f7e8 20px, #f0f7e8 40px)",
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
